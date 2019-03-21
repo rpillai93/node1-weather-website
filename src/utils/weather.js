4c425c4c07f207506bfc5b94a80d25b48df20dff
Rahul Pillai <rahul.pillai93@gmail.com>
2019-03-21 14:13:18 -0400

    Added temperature hi and low

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -11,8 +11,13 @@
               var tempF=body.currently.temperature;
                var tempC=parseFloat((tempF-32)/1.8).toFixed(2);
                var condition = `the surrounding weather conditions for "${results.address}" suggest: ${body.hourly.summary}`;
+               var FtempHi=body.daily.data[0].temperatureHigh;
+               var FtempLo=body.daily.data[0].temperatureLow;
+               var CtempHi=parseFloat((FtempHi-32)/1.8).toFixed(2);
+               var CtempLo=parseFloat((FtempLo-32)/1.8).toFixed(2);
 
-               resolve({
+               resolve({CtempHi,
+                 CtempLo,
                 condition,
                  tempC,
                  results
