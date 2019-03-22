0682d234e150d52f70ed9e94998243d16f38e4b7
Rahul Pillai <rahul.pillai93@gmail.com>
2019-03-22 03:03:21 -0400

    changed geocode api to google

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -7,7 +7,7 @@ var geocodeAddress = (address) => {
   return new Promise((resolve, reject) => {
     var encodedAddress = encodeURIComponent(address);
     request({
-      url: `http://www.mapquestapi.com/geocoding/v1/address?key=DEPUk69fAPkFQ9CCwuD4ux6CTVhgdsWM&location=${encodedAddress}`,
+      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBjwzZIm-zdLgAVMYC4jQr9AsWzOKc8F2Y`,
       json: true
     }, (error, response, body) => {
 
@@ -15,16 +15,16 @@ var geocodeAddress = (address) => {
 
         reject('The MapQuest API service is not communicating.');
 
-      else if (address.length === 0 || body === undefined || body.results[0].locations[0].geocodeQualityCode === 'A1XAX')
+      else if (address.length === 0 || body === undefined || body.results.length===0 || body.status.localeCompare("OK"))
         reject('The location address could not be found or was vague. Please provide proper address details.');
       else {
         // console.log(body.results[0].providedLocation.location);
         // console.log(body.results[0].locations[0].latLng.lat);
         // console.log(body.results[0].locations[0].latLng.lng);
         resolve({
-          address: body.results[0].providedLocation.location,
-          latitude: body.results[0].locations[0].latLng.lat,
-          longitude: body.results[0].locations[0].latLng.lng
+          address: body.results[0].formatted_address,
+          latitude: body.results[0].geometry.location.lat,
+          longitude: body.results[0].geometry.location.lng
         });
 
       }
